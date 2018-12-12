b723f4c441dd25715ac22d38582b2c22d5f495cd
Anthony <anthony-34400@hotmail.fr>
2018-12-12 18:01:56 +0100

    voice recognizer ready. refact and cleanup.

--- mobile/src/main/java/fr/harkame/tp1/fragment/voice/VoiceRecognition.kt
+++ mobile/src/main/java/fr/harkame/tp1/fragment/voice/VoiceRecognition.kt
@@ -1,12 +1,11 @@
 package fr.harkame.tp1.fragment.voice
 
+import android.support.v4.app.Fragment
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +14,8 @@ import android.widget.Button
 import android.widget.TextView
 import fr.harkame.tp1.R
 import fr.harkame.tp1.R.layout.fragment_voice_recognition
+import fr.harkame.tp1.fragment.creation.EventCreationFragment
+import fr.harkame.tp1.fragment.home.HomeFragment
 
 
 class VoiceRecognition: Fragment() {
@@ -22,8 +23,6 @@ class VoiceRecognition: Fragment() {
 
     companion object {
       private const val TAG = "VoiceRecoFragment"
-      private const val res = "Result : "
-      private const val REQUEST_SPEECH_RECOGNIZER = 3000
   }
 
     private lateinit var speakButton: Button
@@ -47,7 +46,7 @@ class VoiceRecognition: Fragment() {
         speakButton = view.findViewById<Button>(R.id.speakButton)
         mText = view.findViewById<TextView>(R.id.voiceText)
 
-        speakButton.setOnClickListener { speakButtonClicked(view) }
+        speakButton.setOnClickListener { speakButtonClicked() }
         recognizer = SpeechRecognizer.createSpeechRecognizer(activity)
         //recognizer.setRecognitionListener(voiceListener())
 
@@ -56,10 +55,10 @@ class VoiceRecognition: Fragment() {
     }
 
     fun verifyRecognitionService(){
-        var pm: PackageManager = activity?.packageManager!!
+        val pm: PackageManager = activity?.packageManager!!
         Log.d("PackageManager : ",pm.toString())
 
-        var activities = pm.queryIntentActivities(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),0)
+        val activities = pm.queryIntentActivities(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),0)
         if(activities.size == 0) {
             speakButton.isEnabled = false
             speakButton.text = "Recognizer not present"
@@ -67,19 +66,55 @@ class VoiceRecognition: Fragment() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            var matches = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            for(matche in matches!!){
-                Log.d("match : ", matche)
-                if(matche == "création"){
-                    mAnswer = matche
-                }
+        val matches = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+
+        for(matche in matches!!){
+
+            Log.d("match : ", matche)
+            val words = matche.split(" ")
+
+            if(words.contains("création")){
+                mAnswer = "création"
+                break
             }
+            else if(words.contains("accueil") || words.contains("d\'accueil")){
+                mAnswer = "accueil"
+                break
+            }
+            else {
+                mAnswer = matche
+            }
+        }
+
         mText.text = mAnswer
+
+        if(mAnswer != "création" || mAnswer!= "accueil"){
+
+            var fragment = Fragment()
+            Log.d("mAnswer : ", mAnswer)
+
+            if(mAnswer == "création"){
+                fragment = EventCreationFragment()
+                Log.d("Fragment ", fragment.toString())
+            }
+            else if (mAnswer == "accueil"){
+                fragment = HomeFragment()
+                Log.d("Fragment ", fragment.toString())
+            }
+
+            val fragmentManager = activity?.supportFragmentManager
+
+            val fragmentTransaction = fragmentManager
+                    ?.beginTransaction()
+                    ?.replace(R.id.fragment_container,fragment)
+                    ?.commit()
+
+        }
 
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    fun speakButtonClicked(view: View){
+    private fun speakButtonClicked(){
         Log.d("DEBUG :" , "Button Clicked !")
         startVoiceRecognitionActivity()
     }
@@ -89,58 +124,7 @@ class VoiceRecognition: Fragment() {
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test")
 
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5)
         startActivityForResult(intent, 1234)
-    }
-
-
-
-    inner class voiceListener :RecognitionListener{
-        override fun onReadyForSpeech(params: Bundle?) {
-            Log.d(TAG, "OnReadyForSpeach")
-        }
-
-        override fun onRmsChanged(rmsdB: Float) {
-            Log.d(TAG, "onRmsChanged")
-        }
-
-        override fun onBufferReceived(buffer: ByteArray?) {
-            Log.d(TAG, "onBufferReceived")
-        }
-
-        override fun onPartialResults(partialResults: Bundle?) {
-            Log.d(TAG, "onPartialResults")
-        }
-
-        override fun onEvent(eventType: Int, params: Bundle?) {
-            Log.d(TAG, "onEvent " + eventType)
-        }
-
-        override fun onBeginningOfSpeech() {
-            Log.d(TAG, "onBeginningOfSpeech");
-        }
-
-        override fun onEndOfSpeech() {
-            Log.d(TAG, "onEndofSpeech")
-        }
-
-        override fun onError(error: Int) {
-            Log.d(TAG,"error : " + error)
-
-        }
-
-        override fun onResults(results: Bundle?) {
-            var str = ""
-            Log.d(TAG, "onResults " + results)
-
-            val datas = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-            if(datas?.size != 0){
-                for(data in datas!!){
-                    Log.d(TAG, "Result : " + data)
-                    str += data
-                }
-                mAnswer = str
-            }
-        }
     }
 }
